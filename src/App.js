e4d34a4bbc8e4e95cb60d66cedadb535d49a81f2
pharmluka <99403991+pharmluka@users.noreply.github.com>
2022-06-27 01:12:50 -0700

    simple react js weather app
    simple weather app in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
+ import { FaTemperatureLow } from 'react-icons/fa';
 
-function App() {
+const App = () => {
+  
+const [query, setQuery] = useState('');
+const [cond, setCond] = useState([]);
+  const url = `http://api.weatherstack.com/current?access_key=9167cbe6b1ef09d19e3296b3834d0860&query=${query}`;
+ const getData = async() => {
+  const result = await axios.get(url);
+  setCond([result.data])
+  console.log(result.data)
+
+ }
+const onSubmit = (e) => {
+  e.preventDefault();
+  getData();
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App text-center m-auto'>
+<form  onSubmit={onSubmit}>
+  <input type='text' value={query} onChange={e => setQuery(e.target.value)} />
+  <button type='submit' className='button-1 '>Enter</button>
+</form>
+<div  className='box text-center '>
+{
+  cond.map((item,index) => {
+
+    return(
+
+      <div key={index} className=' flex m-4 p-4 text-white'>
+      <div className='flex-none'>{item.current.temperature} ° C <FaTemperatureLow /> </div>
+      <div className='flex-auto w-64'>{item.current.weather_descriptions}</div>
+      <div className='flex-auto w-64'><img src={item.current.weather_icons} alt='image' className='object-cover' /></div>
+      <div className='flex-auto w-64'>{item.location.country}</div>
+      <div className='flex-auto w-72'>{item.location.localtime}</div>
+          </div>
+    )
+ 
+  })
+}
+</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
